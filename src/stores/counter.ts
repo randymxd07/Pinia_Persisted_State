83edabee908d7651979c03f52b615fabd939da4d
randymxd06 <randym0624@gmail.com>
2024-03-04 14:15:18 -0400

    feat: :tada: First commit

---
+++ src/stores/counter.ts
@@ -0,0 +1,18 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useCounterStore = defineStore('counter', () => {
+
+  const count = ref<number>(0);
+
+  const increment = () => {
+    count.value++;
+  }
+  
+  const decrement = () => {
+    count.value--;
+  }
+
+  return { count, increment, decrement }
+
+}, { persist: true });
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter';
+import { storeToRefs } from 'pinia';
+const counterStore = useCounterStore();
+const { count } = storeToRefs(counterStore);
+</script>
+
+<template>
+
+  <section>
+
+    <h1>Count: {{ count }}</h1>
+  
+    <button @click="counterStore.decrement" class="btn btn-outline-success px-5 me-2">
+      -
+    </button>
+
+    <button @click="counterStore.increment" class="btn btn-outline-success px-5">
+      +
+    </button>
+
+  </section>
+
+</template>
